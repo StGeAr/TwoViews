0b28c0aaffe0f1f0383d1cd2fdd6672e2b08cf0a
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-02 03:12:11 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Герман Ставицкий on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
